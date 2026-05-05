2fb708acce72f18564392658c83c17c4ed13dc7e
DMilan05 <dmilan.felvi@gmail.com>
2026-05-05 18:50:02 +0200

    Token működik.

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,6 @@ app.use(express.json());
 app.use('/api', authRoutes);
 app.use('/api/favorites', favoriteRoutes);
 
-
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch((err) => console.error('MongoDB connection error:', err));
